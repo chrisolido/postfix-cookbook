ea6b391a5dedf89c171844581db73d2175d462f7
Mike Heffner <mikeh@fesnel.com>
2012-01-23 11:58:42 -0500

    Initial import.

---
+++ recipes/forward.rb
@@ -0,0 +1,43 @@
+#
+# Setup postfix to forward mail to an external SMTP
+#
+
+include_recipe 'postfix'
+
+service "postfix" do
+  supports :status => true
+  action [ :enable, :start ]
+end
+
+# Postfix configuration file
+template "/etc/postfix/main.cf" do
+  source "postfix_main.cf.erb"
+  mode "0600"
+  variables({
+              :hostname => node[:postfix][:smtp_host],
+            })
+  notifies :restart, resources(:service => "postfix")
+end
+
+# Postfix user info file
+template "/etc/postfix/sasl_passwd" do
+  source "postfix_sasl_passwd.erb"
+  mode "0600"
+  variables({
+              :hostname => node[:postfix][:smtp_host],
+              :user => node[:postfix][:sasl_user],
+              :password => node[:postfix][:sasl_password]
+            })
+  notifies :restart, resources(:service => "postfix")
+end
+
+# Compile user info file
+script "compile_sasl_passwd" do
+  interpreter "bash"
+  user "root"
+  cwd "/etc/postfix"
+  code <<-EOH
+      postmap hash:/etc/postfix/sasl_passwd
+    EOH
+  notifies :restart, resources(:service => "postfix")
+end
